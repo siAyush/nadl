e4969dd72a29c7fc11cc51ccce52aa25d8863c51
ayush <atulg7516@gmail.com>
2021-05-24 23:23:53 +0530

    tensor checking

--- nadl/tensor.py
+++ nadl/tensor.py
@@ -1,5 +1,21 @@
-from typing import List
+from typing import Callable, List
 import numpy as np
+from typing import List, NamedTuple, Callable, Optional, Union
+
+
+class Dependency(NamedTuple):
+    tensor: "Tensor"
+    grad_fun: Callable[[np.ndarray], np.ndarray]
+
+
+Arrayable = Union[float, list, np.ndarray]
+
+
+def ensure_array(arrayable: Arrayable) -> np.ndarray:
+    if isinstance(arrayable, np.ndarray):
+        return arrayable
+    else:
+        return np.array(arrayable)
 
 
 class Tensor():
@@ -9,5 +25,9 @@ class Tensor():
                  depends_on: List[Dependency] = None) -> None:
         self.data = data
         self.requires_grad = requires_grad
-        self.depends_on = depends_on
+        self.depends_on = depends_on or []
         self.shape = data.shape
+        self.grad = None
+
+    def __repr__(self) -> str:
+        return f"Tensor({self.data}, requires_grad={self.requires_grad})"
